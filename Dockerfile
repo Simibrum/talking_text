0ebc0bfb0ec9234210bb6f0c81cfdb34c7e61741
Ben Hoyle <benjhoyle@gmail.com>
2023-09-13 13:50:41 +0100

    First commit

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+# Dockerfile for Development Environment with GPU Support
+
+# Get Ubuntu server image with GPU support - just in case we need it later
+# CUDA version needs to be lower than the local CUDA version
+FROM nvidia/cuda:12.2.0-devel-ubuntu22.04
+
+# Set time zone parameters for tzdata
+ARG DEBIAN_FRONTEND="noninteractive"
+ENV TZ="Europe/London"
+
+# Three lines below can be used to install Python 3.11 but this seems incompatible with tprchvision
+# RUN apt-get update
+# RUN apt-get install software-properties-common -y
+# RUN add-apt-repository ppa:deadsnakes/ppa
+RUN apt-get update
+RUN apt-get install --yes --no-install-recommends \
+    openssh-client \
+    git \
+    wget \
+    curl \
+    ca-certificates \
+    python3.10 \
+    python3.10-dev \
+    python3.10-distutils \
+    python3.10-venv \
+    build-essential \
+    libpq-dev
+RUN apt-get clean
+
+# Create virtual environment so we use the right pip etc
+RUN python3.10 -m venv /opt/venv
+ENV PATH="/opt/venv/bin:$PATH"
+RUN pip3 --version
+
+# Install pip and python tools
+RUN pip3 install -U pip setuptools wheel
+
+# Copy over requirements and install first to cache if we only have code change
+COPY requirements.txt talking_text/requirements.txt
+
+# Install pip dependencies
+RUN cd talking_text && pip3 install --no-cache-dir -r requirements.txt
+
+# Install other files
+COPY . talking_text
+
+# Rename the .env_docker file to .env if it exists - || true is to ignore error if file doesn't exist
+# RUN mv -f talking_text/.env_docker talking_text/.env || true
+
+# Environment variables are set in the docker-compose file
+# Set working directory (this should really be /usr/src/app but hey ho)
+# We should also put this up the top but I can't be arsed to rebuild the container
+WORKDIR /talking_text
---
+++ main.py
@@ -0,0 +1,72 @@
+import logging
+
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+
+from langchain.llms.openai import OpenAIChat
+from langchain.chains import RetrievalQA
+from langchain.callbacks import StdOutCallbackHandler
+
+# Create a logger
+logger = logging.getLogger(__name__)
+
+# Create a local file store to cache the embeddings
+logger.info("Creating local file store")
+store = LocalFileStore("./cache/")
+
+# Initialize the embeddings
+logger.info("Initializing embeddings")
+core_embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+# Load the text data from a CSV file
+logger.info("Loading data")
+loader = CSVLoader(
+    file_path="nheb_bible.csv",
+    csv_args={
+        "delimiter": ",",
+    }
+)
+
+data = loader.load()
+
+# Create a cache backed embeddings
+logger.info("Creating cache backed embeddings")
+embedder = CacheBackedEmbeddings.from_bytes_store(
+    core_embedding_model,
+    store,
+    namespace=core_embedding_model.model_name
+)
+
+# Store embeddings in vector store
+logger.info("Storing embeddings in vector store")
+vectorstore = FAISS.from_documents(data, embedder)
+
+# Instantiate a retriever
+logger.info("Instantiating a retriever")
+retriever = vectorstore.as_retriever()
+
+# Initialize the chatbot
+logger.info("Initializing the chatbot")
+llm = OpenAIChat()
+logger.info("Initialising the output handler")
+handler = StdOutCallbackHandler()
+
+# Initialize the QA chain
+logger.info("Initializing the QA chain")
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    retriever=retriever,
+    callbacks=[handler],
+    return_source_documents=True
+)
+
+# Get a question from the user
+logger.info("Getting a question from the user")
+question = "What is the meaning of life?"
+# Ask the question
+response = qa_chain({"query":question})
+
+print(response)
